d069d9794c30d0518475d88a3df3059a611cef02
shintan777 <tanvi.shinde46@gmail.com>
2020-09-13 14:11:54 +0530

    Makes generate endpoint

--- BackEndApp/v1/views.py
+++ BackEndApp/v1/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import sys
+import os
+import json
+import importlib
 
-# Create your views here.
+sys.path.append('..')
+
+from DLMML.parser import *
+from DLMML.utils import json_to_dict
+
+
+@api_view(['POST'])
+def generate(request):
+    body_unicode = request.body.decode('utf-8')
+    inputs_json = json.loads(body_unicode)
+    inputs = json_to_dict.MakeDict(inputs_json).parse()
+
+    lib = inputs['lib']
+    lang = inputs['lang']
+    parser_path = 'DLMML.parser.'+lang+'.'+lib+'.main'
+    parser = importlib.import_module(parser_path)
+
+    status, error = parser.generate_code(inputs)
+    if status:
+        print("Error", error)
+        msg = error
+    else:
+        print("File generated") 
+        msg = 'file generated'
+
+    return JsonResponse({'message': msg})
+
