3961e12abcd59bf8ff8c431d2685fc87269b9005
Harshit Dhyani <harshitdhyani575@gmail.com>
2020-11-22 13:19:50 +0530

    Update views.py

--- BackEndApp/v1/views.py
+++ BackEndApp/v1/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics, permissions
+from knox.models import AuthToken
+from .serializers import UserSerializer, RegisterSerializer
 import sys
 import os
 import json
@@ -65,3 +68,18 @@ def compile(request):
     except Exception as e:
         status, error = 1, e
     return JsonResponse({'status':status, 'error':error})
+
+
+
+# Register API
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+        "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        "token": AuthToken.objects.create(user)[1]
+        })
