e0a92dc1dc08fda4b1333776fa6c2363933c0e3e
Harshit Dhyani <harshitdhyani575@gmail.com>
2020-11-22 13:24:08 +0530

    Update urls.py

--- BackEndApp/v1/urls.py
+++ BackEndApp/v1/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import RegisterAPI
+from .views import RegisterAPI,LoginAPI
+from knox import views as knox_views
+
+
 
 urlpatterns = [
     path('generate/', views.generate, name='generate'),
     path('train/', views.train, name='train'),
     path('compile/', views.compile, name='compile'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 
     path('api/register/', RegisterAPI.as_view(), name='register'),
 ]
