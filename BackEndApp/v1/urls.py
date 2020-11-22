0d714cf973c034c8357fc3b6d1f6aca4d192d43b
Harshit Dhyani <harshitdhyani575@gmail.com>
2020-11-22 13:33:03 +0530

    Update urls.py

--- BackEndApp/v1/urls.py
+++ BackEndApp/v1/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('api/login/', LoginAPI.as_view(), name='login'),
     path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
-
     path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
