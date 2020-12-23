87b905a1f9775e790d551c6eeb04d495ee342b11
Aditya Srivastava <39651310+ADI10HERO@users.noreply.github.com>
2020-12-23 18:12:15 +0530

    Update BackEndApp/authv1/models.py

--- BackEndApp/authv1/models.py
+++ BackEndApp/authv1/models.py
@@ -28,7 +28,7 @@ class User():
 
         regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
         if (not re.search(regex, self.attributes.get('email'))):
-            raise ValueError("Email ivalid")
+            raise ValueError("Invalid email or email already exists")
 
         if self.find():
             raise ValueError("Username invalid")
