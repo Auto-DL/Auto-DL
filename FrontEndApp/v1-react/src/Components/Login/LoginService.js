166bd4ef0aa2383f3bdda4cb4015814a3f47591b
RusherRG <rushang101@gmail.com>
2021-07-15 23:59:58 +0530

    fix: check NODE_ENV for backend url

--- FrontEndApp/v1-react/src/Components/Login/LoginService.js
+++ FrontEndApp/v1-react/src/Components/Login/LoginService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL || "";
+const baseurl = process.env.NODE_ENV == "production" ? "/api" : "";
+const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL || baseurl;
 
 class LoginService {
   async login(data) {
