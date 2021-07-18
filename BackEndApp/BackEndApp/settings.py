1ba4cef54bda755980902057d3ea07e21e76823b
Shubh <shubh.jhawar@galaxyweblinks.in>
2021-07-18 13:32:04 +0530

    worked to putting a looger in the system

--- BackEndApp/BackEndApp/settings.py
+++ BackEndApp/BackEndApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import logging
 import logging.config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
