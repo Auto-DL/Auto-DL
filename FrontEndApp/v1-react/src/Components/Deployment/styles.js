5273c9d671b32827fd58b380a4f8c744e28bb89f
Mcvean Soans <mc.soans02@gmail.com>
2021-08-16 09:40:37 +0530

    feat: deployments initial layout

---
+++ FrontEndApp/v1-react/src/Components/Deployment/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles({
+    table: {
+        minWidth: 700,
+    },
+    title: {
+        float: "left",
+        fontSize: "24px",
+        fontWeight: "700",
+    },
+    hover: {
+        cursor: "pointer",
+    },
+    floatright: {
+        float: "right",
+        cursor: "pointer",
+        padding: "3px",
+    },
+    typo: {
+        paddingBottom: "10px",
+    },
+});
--- FrontEndApp/v1-react/src/Components/MainApp/Layout.js
+++ FrontEndApp/v1-react/src/Components/MainApp/Layout.js
@@ -17,6 +17,8 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
 import HomeIcon from "@material-ui/icons/Home";
+import Icon from "@material-ui/core/Icon";
+import SvgIcon from '@material-ui/core/SvgIcon';
 import { useHistory } from "react-router-dom";
 const drawerWidth = 240;
 
@@ -103,6 +105,13 @@ function Layout() {
   const home = () => {
     console.log("home");
     history.push("/home");
+    handleDrawerClose();
+  };
+
+  const deploy = () => {
+    console.log("deployments");
+    history.push("/deploy");
+    handleDrawerClose();
   };
 
   const logout = () => {
@@ -164,7 +173,9 @@ function Layout() {
               )}
             </IconButton>
           </div>
+
           <Divider />
+
           <List>
             <ListItem button onClick={home} key={"Home"}>
               <ListItemIcon>
@@ -173,6 +184,23 @@ function Layout() {
               <ListItemText primary={"Home"} />
             </ListItem>
           </List>
+
+          <Divider />
+
+          <List>
+            <ListItem button onClick={deploy} key={"Deploy"}>
+              <ListItemIcon>
+                <Icon aria-label="deploy">
+                  <SvgIcon viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M20 22L16.14 20.45C16.84 18.92 17.34 17.34 17.65 15.73L20 22M7.86 20.45L4 22L6.35 15.73C6.66 17.34 7.16 18.92 7.86 20.45M12 2C12 2 17 4 17 12C17 15.1 16.25 17.75 15.33 19.83C15 20.55 14.29 21 13.5 21H10.5C9.71 21 9 20.55 8.67 19.83C7.76 17.75 7 15.1 7 12C7 4 12 2 12 2M12 12C13.1 12 14 11.1 14 10C14 8.9 13.1 8 12 8C10.9 8 10 8.9 10 10C10 11.1 10.9 12 12 12Z" />
+                  </SvgIcon>
+                </Icon>
+                {/* <span class="iconify" data-icon="mdi:rocket-launch"></span> */}
+              </ListItemIcon>
+              <ListItemText primary={"Deploy"} />
+            </ListItem>
+          </List>
+
           <Divider />
 
           <List>
@@ -188,6 +216,7 @@ function Layout() {
               <ListItemText primary={"Logout"} />
             </ListItem>
           </List>
+
         </Drawer>
       ) : null}
       <main className={classes.content}>
