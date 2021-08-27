c4a1a079f1270c35de230cde0ae83af5f2427f5f
mahekn23 <2018.mahek.nagdev@ves.ac.in>
2021-08-27 10:41:33 +0530

    updated test_session
    Signed-off-by: mahekn23 <2018.mahek.nagdev@ves.ac.in>

--- BackEndApp/tests/test_session.py
+++ BackEndApp/tests/test_session.py
@@ -9,11 +9,11 @@ test_session = TestSession()
 
 def test_create(mocker):
     mock_os_obj.mock_mongodb_uri(mocker)
-
     session_obj = test_session.create_session()
     test_token = session_obj.create()
     assert test_token is not None
 
+    mock_os_obj.mock_mongodb_uri(mocker)
     session_obj_none = test_session.create_none_session()
     test_token_none = session_obj_none.create()
     assert test_token_none is None
@@ -21,12 +21,12 @@ def test_create(mocker):
 
 def test_delete(mocker):
     mock_os_obj.mock_mongodb_uri(mocker)
-
     session_obj = test_session.create_session()
     test_token = session_obj.create()
     return_value = session_obj.delete(test_token)
     assert return_value == True
 
+    mock_os_obj.mock_mongodb_uri(mocker)
     session_obj_none = test_session.create_none_session()
     test_token_none = session_obj_none.create()
     return_value = session_obj_none.delete(test_token_none)
@@ -35,12 +35,12 @@ def test_delete(mocker):
 
 def test_find(mocker):
     mock_os_obj.mock_mongodb_uri(mocker)
-
     session_obj = test_session.create_session()
     test_token = session_obj.create()
     return_token = session_obj.find(test_token)
     assert return_token["token"] == test_token
 
+    mock_os_obj.mock_mongodb_uri(mocker)
     session_obj_none = test_session.create_none_session()
     test_token_none = session_obj_none.create()
     return_token_none = session_obj_none.find(test_token_none)
@@ -49,12 +49,12 @@ def test_find(mocker):
 
 def test_verify(mocker):
     mock_os_obj.mock_mongodb_uri(mocker)
-
     session_obj = test_session.create_session()
     test_token = session_obj.create()
     return_value = session_obj.verify(test_token)
     assert return_value == True
 
+    mock_os_obj.mock_mongodb_uri(mocker)
     session_obj_none = test_session.create_none_session()
     test_token_none = session_obj_none.create()
     return_value = session_obj_none.verify(test_token_none)
