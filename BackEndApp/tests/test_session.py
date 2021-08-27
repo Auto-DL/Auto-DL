4a3f5e5e4c228817077d449d1c9d9c419462d668
mahekn23 <2018.mahek.nagdev@ves.ac.in>
2021-08-27 12:20:32 +0530

    updated test_session
    Signed-off-by: mahekn23 <2018.mahek.nagdev@ves.ac.in>

--- BackEndApp/tests/test_session.py
+++ BackEndApp/tests/test_session.py
@@ -4,29 +4,32 @@ from .utils_tests import TestSession
 from tests.mocks import MockOS
 
 mock_os_obj = MockOS()
-test_session = TestSession()
 
 
 def test_create(mocker):
+    mock_os_obj.mock_jwt_secret(mocker)
     mock_os_obj.mock_mongodb_uri(mocker)
+    test_session = TestSession()
+
     session_obj = test_session.create_session()
     test_token = session_obj.create()
     assert test_token is not None
 
-    mock_os_obj.mock_mongodb_uri(mocker)
     session_obj_none = test_session.create_none_session()
     test_token_none = session_obj_none.create()
     assert test_token_none is None
 
 
 def test_delete(mocker):
+    mock_os_obj.mock_jwt_secret(mocker)
     mock_os_obj.mock_mongodb_uri(mocker)
+    test_session = TestSession()
+
     session_obj = test_session.create_session()
     test_token = session_obj.create()
     return_value = session_obj.delete(test_token)
     assert return_value == True
 
-    mock_os_obj.mock_mongodb_uri(mocker)
     session_obj_none = test_session.create_none_session()
     test_token_none = session_obj_none.create()
     return_value = session_obj_none.delete(test_token_none)
@@ -34,13 +37,15 @@ def test_delete(mocker):
 
 
 def test_find(mocker):
+    mock_os_obj.mock_jwt_secret(mocker)
     mock_os_obj.mock_mongodb_uri(mocker)
+    test_session = TestSession()
+
     session_obj = test_session.create_session()
     test_token = session_obj.create()
     return_token = session_obj.find(test_token)
     assert return_token["token"] == test_token
 
-    mock_os_obj.mock_mongodb_uri(mocker)
     session_obj_none = test_session.create_none_session()
     test_token_none = session_obj_none.create()
     return_token_none = session_obj_none.find(test_token_none)
@@ -48,13 +53,15 @@ def test_find(mocker):
 
 
 def test_verify(mocker):
+    mock_os_obj.mock_jwt_secret(mocker)
     mock_os_obj.mock_mongodb_uri(mocker)
+    test_session = TestSession()
+
     session_obj = test_session.create_session()
     test_token = session_obj.create()
     return_value = session_obj.verify(test_token)
     assert return_value == True
 
-    mock_os_obj.mock_mongodb_uri(mocker)
     session_obj_none = test_session.create_none_session()
     test_token_none = session_obj_none.create()
     return_value = session_obj_none.verify(test_token_none)
