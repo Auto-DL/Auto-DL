bedd591fe2ceed945550012cb1da6bb6256d23a1
Rushang Gajjal <rushang101@gmail.com>
2021-09-30 19:07:19 +0530

    feat: add GitHub workflows for conventional checks (#341)
    * feat: add commitlint workflow
    
    Signed-off-by: RusherRG <rushang101@gmail.com>
    
    * feat: add PR lint workflow
    
    Signed-off-by: RusherRG <rushang101@gmail.com>
    
    * docs: update PR naming convention in CONTRIBUTING.md and verbose output for workflow
    
    Signed-off-by: RusherRG <rushang101@gmail.com>
    
    * fix: update event trigger of commitlint to only push
    
    Signed-off-by: RusherRG <rushang101@gmail.com>

---
+++ .github/configurations/commitlint.config.js
@@ -0,0 +1,47 @@
+const Configuration = {
+    /*
+     * Resolve and load @commitlint/config-conventional from node_modules.
+     * Referenced packages must be installed
+     */
+    extends: ['@commitlint/config-conventional'],
+    /*
+     * Resolve and load conventional-changelog-atom from node_modules.
+     * Referenced packages must be installed
+     */
+    parserPreset: './parser-preset',
+    /*
+     * Resolve and load @commitlint/format from node_modules.
+     * Referenced package must be installed
+     */
+    formatter: '@commitlint/format',
+    /*
+     * Any rules defined here will override rules from @commitlint/config-conventional
+     */
+    rules: {},
+    /*
+     * Functions that return true if commitlint should ignore the given message.
+     */
+    ignores: [(commit) => commit === ''],
+    /*
+     * Whether commitlint uses the default ignore rules.
+     */
+    defaultIgnores: true,
+    /*
+     * Custom URL to show upon failure
+     */
+    helpUrl:
+        'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+    /*
+     * Custom prompt configs
+     */
+    prompt: {
+        messages: {},
+        questions: {
+            type: {
+                description: 'please input type:',
+            },
+        },
+    },
+};
+
+module.exports = Configuration;
