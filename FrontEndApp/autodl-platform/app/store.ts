1d7f6b6fe6e0ac7df89d51bf5d94fa24c020c464
RusherRG <rushang101@gmail.com>
2021-10-03 03:06:37 +0530

    feat: configure redux store
    Signed-off-by: RusherRG <rushang101@gmail.com>

---
+++ FrontEndApp/autodl-platform/app/store.ts
@@ -0,0 +1,17 @@
+import { Action, configureStore, ThunkAction, } from "@reduxjs/toolkit";
+import authReducer from "components/AuthForm/AuthSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
