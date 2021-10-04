6782a888e7aee8df5344df6262199157b8975893
priyanshm07 <priyanshmaheshwari94@gmail.com>
2021-10-04 22:16:13 +0530

    Payment integration Component Nextjs

--- FrontEndApp/autodl-platform/components/SideBar/SideBar.tsx
+++ FrontEndApp/autodl-platform/components/SideBar/SideBar.tsx
@@ -12,6 +12,14 @@ import Settings from '@material-ui/icons/Settings';
 import Routes from 'utils/routes';
 import Router from 'next/router';
 
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import Modal from '@material-ui/core/Modal';
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import InputAdornment  from "@material-ui/core/InputAdornment"
+
 type Props = {
   activeTab?: string;
   projectName?: string | string[] | undefined;
@@ -28,7 +36,8 @@ const useStyles = makeStyles((theme: Theme) =>
       flexGrow: 1,
     },
     icon: {
-      color: theme.palette.background.default,
+      color: "white",
+      // color: theme.palette.background.default,
       opacity: '0.6',
       fontSize: '20px'
     },
@@ -67,10 +76,37 @@ const useStyles = makeStyles((theme: Theme) =>
         width: theme.spacing(7) + 1,
       },
     },
+    modalBox: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: 600,
+      backgroundColor: 'white',
+      border: '1px solid #000',
+      borderRadius: '5px',
+      boxShadow: "24px",
+      padding: "20px",
+      textAlign: 'center',
+      display: 'flex',
+      flexDirection: 'column',        
+    },
+    donate: {
+      width: "80px"
+  }
   }),
 );
 
 export default function SideBar({ activeTab, projectName }: Props) {
+
+  const defaultDonateAmount = [50, 100, 150, 200, ]
+
+  const [donateModalOpen, setDonateModalOpen] = React.useState(false);
+  const modalOpen = () => setDonateModalOpen(true);
+  const modalClose = () => setDonateModalOpen(false);
+
+  const [donateAmt, setDonateAmt] = React.useState({amt:"0"});
+  
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
@@ -81,6 +117,12 @@ export default function SideBar({ activeTab, projectName }: Props) {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
+  const displayRazorpay = (amount: string) => {
+    alert(amount)
+    setDonateAmt({amt: "0"})
+    modalClose()
+  }
 
   return (
     <Drawer
@@ -110,8 +152,8 @@ export default function SideBar({ activeTab, projectName }: Props) {
               [classes.inactive]: route.name != activeTab,
             })}
           >
-            <ListItemIcon style={{ color: 'inherit' }}><route.icon /></ListItemIcon>
-            <ListItemText primary={route.name} />
+            <ListItemIcon style={{ color: 'white' }}><route.icon /></ListItemIcon>
+            <ListItemText primary={route.name} style={{color: "white"}}/>
           </ListItem>
         ))}
       </List>
@@ -123,6 +165,57 @@ export default function SideBar({ activeTab, projectName }: Props) {
           <ListItemText primary='Settings' style={{ color: 'white' }} />
         </ListItem>
       </List>
+
+      <ListItem button key='' onClick={modalOpen}>
+          <ListItemIcon><AttachMoneyIcon style={{ color: 'white' }} /></ListItemIcon>
+          <ListItemText primary='Donate Us' style={{ color: 'white' }} />
+      </ListItem>
+
+      <Modal
+        open={donateModalOpen}
+        onClose={modalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className={clsx(classes.modalBox)}>
+          <Typography id="modal-modal-title" variant="h5" component="h3" style={{paddingBottom: "10px"}}>
+            Support Auto-DL
+          </Typography>
+          <Typography id="modal-modal-description">
+          Auto-DL helps you make Deep Learning models without writing a single line of code and giving as little input as possible.
+        </Typography>
+        <div style={{display: 'flex', justifyContent: 'space-between', fontSize: '20px', margin: '30px'}}>
+          {defaultDonateAmount.map((amount) => 
+          <TextField className={clsx(classes.donate)}
+            onClick={() => setDonateAmt({amt: `${amount}`})}
+            id="standard-read-only-input"
+            variant="outlined"
+            value={amount}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">₹</InputAdornment>,
+              readOnly: true,
+            }}
+            />
+          )}
+          <TextField 
+            style={{width: 100}}
+            type="Number"
+            label="Amount"
+            onChange={(e) => setDonateAmt({amt: e.target.value})}
+            className={clsx(classes.donate)}
+            id="outlined-number"
+            placeholder="Other"
+            variant="outlined"
+            InputProps={{
+              startAdornment: <InputAdornment position="start">₹</InputAdornment>,
+            }}
+          />
+
+        </div>
+        <Button onClick={() => displayRazorpay(donateAmt.amt)} type="submit" variant="contained" color="primary" >Donate</Button>
+        
+        </Box>
+      </Modal>
     </Drawer>
   );
 }
