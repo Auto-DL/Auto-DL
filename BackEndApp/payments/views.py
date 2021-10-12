b8c2ce67e067cd8c833ca353ce1f69f7078f5a7d
priyanshm07 <priyanshmaheshwari94@gmail.com>
2021-10-12 22:26:52 +0530

    feat django payment integration

--- BackEndApp/payments/views.py
+++ BackEndApp/payments/views.py
@@ -6,18 +6,19 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import os
-from payments.utils import get_db_handle
-MONGODB_URI = os.getenv('MONGODB_URI')
-db_handle, mongo_client = get_db_handle('AutoDl', MONGODB_URI, "", "")
+# from payments.utils import get_db_handle
+# MONGODB_URI = os.getenv('MONGODB_URI')
+# db_handle, mongo_client = get_db_handle('AutoDl', MONGODB_URI, "", "")
 
 RAZORPAY_API_KEY = os.getenv('RAZORPAY_API_KEY')
 RAZORPAY_API_SECRET = os.getenv('RAZORPAY_API_SECRET')
 FRONTEND_HOST = os.getenv('FRONTEND_HOST')
 
+client = razorpay.Client(auth=(RAZORPAY_API_KEY, RAZORPAY_API_SECRET))
+
 @api_view(["POST"])
 def start_payment(request): 
     payableAmount = request.data["amount"]
-    client = razorpay.Client(auth=(RAZORPAY_API_KEY, RAZORPAY_API_SECRET))
 
     payment = client.order.create(
         {
@@ -29,33 +30,25 @@ def start_payment(request):
 
 @api_view(["POST"])
 def verify_payment(request):
-    
-    # res = json.loads(request.data["response"])
-    print("Response: ",request.data)
-    
-    ord_id = ""
-    raz_pay_id = ""
-    raz_signature = ""
-    
-    # for key in res.keys():
-    #     if key == 'razorpay_order_id':
-    #         ord_id = res[key]
-    #     elif key == 'razorpay_payment_id':
-    #         raz_pay_id = res[key]
-    #     elif key == 'razorpay_signature':
-    #         raz_signature = res[key]        
-    try:
-        print("Going Ahead")
-        orderId = request.data.get("razorpay_order_id")
-        paymentId = request.data.get("razorpay_payment_id")
-        userName = "Priyansh"
-        status = 200
-    except:
-        print("Stopped Error")
-        orderId = None
-        userName = None
-        status = 401
-    data = {
-        orderId, userName
-    }
-    return redirect(f'http://localhost:3000/project/paymentSuccess?name={userName}&orderId={orderId}')
+
+    if request.method == "POST": 
+        payment_id = request.POST.get("razorpay_payment_id")
+        razorpay_order_id = request.POST.get("razorpay_order_id")
+        signature = request.POST.get("razorpay_signature")
+
+        details = {
+                'razorpay_order_id': razorpay_order_id,
+                'razorpay_payment_id': payment_id,
+                'razorpay_signature': signature
+            }
+
+        result = client.utility.verify_payment_signature(details)   
+        if result is None:
+            orderDetails = client.order.fetch(razorpay_order_id)
+            userName = "Priyansh"
+            amount = orderDetails["amount"]/100
+        else: 
+            userName = "Not Found"
+            amount = 0
+
+        return redirect(f'http://localhost:3000/project/paymentSuccess?name={userName}&amount={amount}')
