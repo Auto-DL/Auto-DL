c024165941ba72cfb0dc7dfc4304e00c12a6e98d
Priyansh Maheshwari <68078404+priyanshm07@users.noreply.github.com>
2021-10-15 13:27:59 +0530

    Update FrontEndApp/autodl-platform/components/Donate/Donate.tsx
    Final commit for Payment component Nextjs
    
    Co-authored-by: Mcvean Soans <69250084+McTechie@users.noreply.github.com>

--- FrontEndApp/autodl-platform/components/Donate/Donate.tsx
+++ FrontEndApp/autodl-platform/components/Donate/Donate.tsx
@@ -13,56 +13,60 @@ import InputAdornment  from "@material-ui/core/InputAdornment"
 // import Axios from "axios";
 
 const useStyles = makeStyles({
-    modalBox: {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 600,
-        backgroundColor: '#e7eef4',
-        border: '1px solid #000',
-        borderRadius: '5px',
-        boxShadow: "24px",
-        padding: "20px",
-        textAlign: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    amountContainer: {
-        display: 'flex', 
-        justifyContent: 'space-between', 
-        fontSize: '20px', 
-        margin: '30px'        
-    },
-    modalTextField: {
-        width: "84px",
-        cursor: "pointer"
-    },
-    buttonContainer:{
-      display: "flex",
-      justifyContent: "space-evenly"
-    }
-  });
-
+  modalBox: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 600,
+    backgroundColor: '#e7eef4',
+    border: '1px solid #000',
+    borderRadius: '5px',
+    boxShadow: '24px',
+    padding: '20px',
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  amount: {
+    width: '100px',
+  },
+  amountContainer: {
+    display: 'flex', 
+    justifyContent: 'space-between', 
+    fontSize: '20px', 
+    marginTop: '30px',
+    marginBottom: '20px',
+  },
+  actionBtn: {
+    width: '180px',
+  },
+  actionBtnContainer:{
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    marginTop: '15px',
+    marginBottom: '10px'
+  }
+});
 
 export default function Donate() {
   const classes = useStyles();
 
-  const defaultDonateAmount = [50, 200, 500, 1000, ]
-
-  type donateAmount = {
-    amt: number
-  }
-  const [donateAmount, setDonateAmt] = React.useState<donateAmount>({ amt: 0 });
+  const defaultDonateAmount = [50, 200, 500, 1000, ];
+  const [donateAmount, setDonateAmt] = React.useState<number>(0);
   const [donateModalOpen, setDonateModalOpen] = React.useState<boolean>(false);
+
   const modalOpen = () => setDonateModalOpen(true);
   const modalClose = () => setDonateModalOpen(false);
 
+  const handleDonateAmount = (amount: number) => {
+    setDonateAmt(amount);
+  }
 
-  const displayRazorpay = (amt:any) => {
-    alert(amt)
-    setDonateAmt({amt: 0})
-    modalClose()
+  const displayRazorpay = (amt: number) => {
+    alert(amt);
+    setDonateAmt(0);
+    modalClose();
   }
 
   return (
@@ -75,55 +79,65 @@ export default function Donate() {
       <Modal
         open={donateModalOpen}
         onClose={modalClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+        aria-labelledby="amount-modal-title"
+        aria-describedby="amount-modal-description"
       >
         <Box className={classes.modalBox}>
-          <Typography id="modal-modal-title" variant="h5" component="h3" style={{paddingBottom: "10px"}}>
+          <Typography id="amount-modal-title" variant="h5" component="h3" style={{paddingBottom: "10px"}}>
             Support Auto-DL
           </Typography>
-          <Typography id="modal-modal-description">
-          We help you make Deep Learning models without writing a single line of code!
-        </Typography>
-        <div className={classes.amountContainer}>
-          {defaultDonateAmount.map((amount:number) => 
-          <TextField 
-            key={amount}
-            className={classes.modalTextField}
-            onClick={() => setDonateAmt({amt: amount})}
-            id="standard-read-only-input"
-            variant="outlined"
-            value={amount}
-            InputProps={{
-              startAdornment: <InputAdornment position="start">₹</InputAdornment>,
-              readOnly: true,
-            }}
-            />
-          )}
-          <TextField 
-            style={{width: 100}}
-            label="Amount"
-            type="number"
-            onChange={
-              (e) => setDonateAmt({amt: (e.target as HTMLInputElement).valueAsNumber})
-            }
-            id="outlined-number"
-            placeholder="Other"
-            variant="outlined"
-            InputProps={{
-              startAdornment: <InputAdornment position="start">₹</InputAdornment>,
-            }}
-          />
 
-        </div>
-        <div className={classes.buttonContainer}>
-          <Button
-            onClick={() => modalClose()}
-            type="submit" variant="outlined" color="primary" >Go Back</Button>
-          <Button
-            onClick={() => displayRazorpay(donateAmount.amt)} 
-            type="submit" variant="contained" color="primary" >Donate</Button>
-        </div>
+          <Typography id="amount-modal-description">
+            We help you make Deep Learning models without writing a single line of code!
+          </Typography>
+
+          <div className={classes.amountContainer}>
+            {defaultDonateAmount.map((amount: number) => 
+              <Button 
+                key={amount}
+                value={amount}
+                className={classes.amount}
+                color="primary"
+                variant={donateAmount === amount ? "contained" : "outlined"}
+                onClick={() => handleDonateAmount(amount)}
+              >
+                ₹ {amount}
+              </Button>
+            )}
+
+            <TextField 
+              className={classes.amount}
+              label="Amount"
+              variant="outlined"
+              placeholder="Other"
+              onChange={
+                (e) => handleDonateAmount(Number(e.target.value))
+              }
+              InputProps={{
+                startAdornment: <InputAdornment position="start">₹</InputAdornment>,
+              }}
+            />
+          </div>
+
+          <div className={classes.actionBtnContainer}>
+            <Button
+              onClick={() => modalClose()}
+              className={classes.actionBtn}
+              color="primary"
+              variant="outlined"
+            >
+                Go Back
+            </Button>
+            <Button
+              onClick={() => displayRazorpay(donateAmount)}
+              className={classes.actionBtn}
+              color="primary"
+              variant="contained"
+              type="submit"
+            >
+              Proceed
+            </Button>
+          </div>
         </Box>
       </Modal>      
     </div>
