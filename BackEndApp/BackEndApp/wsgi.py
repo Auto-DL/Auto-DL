bd9d7d17c76e3467692e7f486e9e61bdd59d7d03
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-10-19 17:30:40 +0530

    added doc strings

--- BackEndApp/BackEndApp/wsgi.py
+++ BackEndApp/BackEndApp/wsgi.py
@@ -1,3 +1,10 @@
+"""
+WSGI config for BackEndApp project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
