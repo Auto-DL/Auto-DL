16665a340e5a028dc61fa79b2c3831e4072721fe
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-10-19 16:42:29 +0530

    docs update

--- BackEndApp/BackEndApp/asgi.py
+++ BackEndApp/BackEndApp/asgi.py
@@ -1,3 +1,10 @@
+"""
+ASGI config for BackEndApp project.
+It exposes the ASGI callable as a module-level variable named ``application``.
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
 import os
 
 from django.core.asgi import get_asgi_application
