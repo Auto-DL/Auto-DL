4ef1c7de5187a24f8d0b7b584e1a9cfd745261ca
Dhruva Shaw <34207400+Dhruvacube@users.noreply.github.com>
2021-10-19 17:17:22 +0530

    improved apps import/load

--- BackEndApp/authv1/apps.py
+++ BackEndApp/authv1/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class Authv1Config(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "authv1"
--- BackEndApp/v1/apps.py
+++ BackEndApp/v1/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class V1Config(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "v1"
