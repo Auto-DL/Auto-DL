dd07bb7d8eb057444c2e402ea4132b009e472284
Ranjeet Singh <techonerd1.0@gmail.com>
2021-10-21 12:12:38 +0530

    test suite for views

--- BackEndApp/authv1/tests/test_views.py
+++ BackEndApp/authv1/tests/test_views.py
@@ -2,9 +2,6 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from authv1.models import User
 
-# from tests.mocks import MockOS
-# import  pytest
-
 
 class TestUser(TestCase):
     def setUp(self):
@@ -12,15 +9,12 @@ class TestUser(TestCase):
         self.client = Client()
         self.register_url = reverse("register")
         self.login_url = reverse("login")
+        self.profile_update_url = reverse("update-profile")
         self.user = User(
             username="testuser1",
             password="Qwerty@123",
             **{"email": "testuser1@xyz.com", "first_name": "test", "last_name": "user1"}
         )
-        # self.user = User.create(
-        #     username='testuser',)
-        # self.user = TestUser()
-        # print(self.user)
 
     # not working, giving error unauthorised
     # def test_user_registration(self):
@@ -29,22 +23,33 @@ class TestUser(TestCase):
     #     assert response.status_code != 200
 
     def test_login(self):
-        user_id = self.user.create()
+        data = {
+            "username": "testuser1",
+            "password": "Qwerty@123",
+        }
         user = self.user.find(by_email=False)
-        response = self.client.post(
-            self.login_url,
-            params={
-                "username": "testuser1",
-                "password": "Qwerty@123",
-            },
-        )
-        assert response.status_code == 200
+        response = self.client.post(self.login_url, data)
+        self.assertEqual(response.status_code, 200)
 
     ## not aplicable until user is login
-    # def test_user_profile_update(self):
-    #     self.client.login(username='testuser', password='testuser')
-    #     response = self.client.post(reverse('update-profile'))
-    #     print(response, self.user)
-    #     self.assertEqual(response.status_code, 200)
+    def test_user_profile_update(self):
+        bad_data = {"username": ""}
+        bad_response = self.client.post(self.profile_update_url, bad_data)
+        existing_data = {"username": "testuser1", "new_email": ""}
+        no_change_response = self.client.post(self.profile_update_url, existing_data)
+        change_email_data = {
+            "username": "testuser1",
+            "new_email": "email_update@xyz.com",
+        }
+        email_update_response = self.client.post(
+            self.profile_update_url, change_email_data
+        )
 
-    # self.assertTemplateUsed(response, 'user_profile_update.html')
+        self.assertEqual(
+            bad_response.json()["message"],
+            "No user found. Please register if visiting first time",
+        )
+        self.assertEqual(no_change_response.json()["message"], "Invalid data")
+        self.assertEqual(
+            email_update_response.json()["message"], "User detail updated successfully"
+        )
